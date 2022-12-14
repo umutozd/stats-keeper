3abb7da77d38709b577f57962ae90be79f251261
Umut Özdoğan <umut@otsimo.com>
2022-12-14 23:21:40 +0300

    protos/statspb: add initial data types

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/umutozd/stats-keeper/protos/statspb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
-	hm := statspb.HelloMessage{
-		Hello: "hello",
-		World: 42,
+	msg := &statspb.ComponentDate{
+		Timestamps: []*timestamppb.Timestamp{
+			timestamppb.New(time.Now()),
+		},
+	}
+	d, err := json.MarshalIndent(msg, "", " ")
+	if err != nil {
+		log.Fatalf("error marshaling message: %v", err)
 	}
 
-	fmt.Printf("Hello: %q, World: %d\n", hm.Hello, hm.World)
+	fmt.Printf("marshaled message: %s", string(d))
 }
